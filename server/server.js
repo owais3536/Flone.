5ea93701a2f4c32a9de550580479e64a962b6182
owais3536 <owais8812@gmail.com>
2025-11-20 12:28:35 +0500

    Add .env file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, }));
 
-app.get("/", (req, res) => {
-    res.send("Working!");
-})
+import connectDB from "./utils/connectDB.js";
 
-app.listen(8080, () => console.log("Server running on localhost:8080"));
+const PORT = 8080 || process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}`);
+    connectDB();
+});
