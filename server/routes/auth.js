c69298415b87b129d2dd132e5b51f9f24fd425f6
owais3536 <owais8812@gmail.com>
2025-11-20 23:21:47 +0500

    Add sign in route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import User from "../models/auth.js";
+import { protectedRoute } from "../utils/middleware.js";
 
 const router = Router();
 
+// registeration
 router.post("/register", async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -24,7 +26,7 @@ router.post("/register", async (req, res) => {
                 .json({ error: true, message: "User already exists with this email" });
         }
 
-        hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = new User({
             name,
@@ -55,6 +57,61 @@ router.post("/register", async (req, res) => {
             error: true,
             message: error.message,
         });
+    }
+});
+
+// sign in
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                error: true,
+                message: "All fields are required",
+            });
+        }
+
+        const isUser = await User.findOne({ email });
+
+        if (!isUser) {
+            return res.status(404).json({
+                error: true,
+                message: "No user found with this Id",
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, isUser.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                error: true,
+                message: "Invalid credentials",
+            });
+        }
+
+        const accessToken = jwt.sign(
+            { userId: isUser._id, role: isUser.role },
+            process.env.SECRET_KEY,
+            { expiresIn: "72h" },
+        );
+
+        res.status(200).json({
+            error: false,
+            message: "Sign in successful",
+            user: {
+                name: isUser.name,
+                email: isUser.email,
+                role: isUser.role,
+            },
+            accessToken,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: error.message,
+        })
     }
 });
 
