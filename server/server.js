eb6592933f2bc49b5b92ee82e5d5c990895ded4d
owais3536 <owais8812@gmail.com>
2025-11-20 23:22:20 +0500

    Update

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
+import connectDB from "./utils/connectDB.js";
+import authRoute from "./routes/auth.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, }));
 
-import connectDB from "./utils/connectDB.js";
+app.use("/api/auth", authRoute);
 
 const PORT = 8080 || process.env.PORT;
 
