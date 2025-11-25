0df9bc203dfe70314a97dae90f83f7b84f5a6b2d
owais3536 <owais8812@gmail.com>
2025-11-26 00:14:57 +0500

    Modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import Mens from './pages/Mens';
 import Kids from './pages/Kids';
@@ -7,31 +7,33 @@ import Cart from "./components/Cart";
 import HomePage from './pages/HomePage';
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import ItemDetail from "./pages/ItemDetail";
-import SignUp_SignIn from "./layout/SignUp_SignIn";
+import DashboardLayout from "./layout/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
+import RootLayout from "./layout/RootLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/mens" element={<Mens />} />
-        <Route path="/womens" element={<Womens />} />
-        <Route path="/kids" element={<Kids />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product-details/:id" element={<ItemDetail />} />
-
-        <Route element={<SignUp_SignIn />}>
+        <Route element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/mens" element={<Mens />} />
+          <Route path="/womens" element={<Womens />} />
+          <Route path="/kids" element={<Kids />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product-details/:id" element={<ItemDetail />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="signin" element={<SignIn />} />
         </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  )
-}
 
-export default App
+        <Route path="dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
