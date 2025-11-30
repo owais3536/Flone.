314f75089267305cd62a76bb4d25cce0ba929d26
owais3536 <owais8812@gmail.com>
2025-11-30 19:04:04 +0500

    Fetch user info

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -49,6 +49,32 @@ export const signin = createAsyncThunk(
     }
 );
 
+export const getUser = createAsyncThunk(
+    "auth/getUser",
+    async () => {
+        try {
+            const token = localStorage.getItem("accessToken");
+            const response = await fetch(`${url}/api/auth/get-user-info`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                return thunkAPI.rejectWithValue(data.message || "User not found");
+            }
+
+            return data.user;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
 export const signout = createAsyncThunk(
     "auth/signout",
     async () => {
@@ -92,6 +118,21 @@ const userSlice = createSlice({
                 state.user = action.payload;
             })
             .addCase(signin.rejected, (state, action) => {
+                state.status = "failed";
+                state.isError = action.payload;
+            });
+
+        builder
+            .addCase(getUser.pending, (state) => {
+                state.status = "pending";
+                state.isLoading = true;
+            })
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.status = "success";
+                state.isLoading = false;
+                state.user = action.payload;
+            })
+            .addCase(getUser.rejected, (state, action) => {
                 state.status = "failed";
                 state.isError = action.payload;
             });
