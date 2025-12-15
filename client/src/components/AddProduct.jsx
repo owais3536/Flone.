af22bd4defede570a752c0105ee29f235ca54c46
owais3536 <owais8812@gmail.com>
2025-12-15 22:26:33 +0500

    Modified

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -1,15 +1,15 @@
 
 const AddProduct = ({ addProduct, setAddProduct }) => {
-    console.log(addProduct);
+
     return <>
         <div
             onClick={() => setAddProduct(!addProduct)}
             className={`fixed top-0 right-0 w-full h-full bg-black/30 transition-all duration-300 ease-in-out z-40
-                ${addProduct ? "opacity-0 pointer-events-none" : "opacity-100 pointer-events-auto"}`}
+                ${addProduct ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
         ></div>
         <aside
             className={`fixed top-0 h-full w-[420px] bg-white px-4 z-50 transition-all duration-300 ease-in-out py-8
-                ${addProduct ? "-right-100" : "right-0"}`}
+                ${addProduct ? "right-0" : "-right-100"}`}
         >
             <h2>Add Product</h2>
 
@@ -22,12 +22,12 @@ const AddProduct = ({ addProduct, setAddProduct }) => {
                 />
             </div>
             <div className="mt-4">
-                <label className="text-xs text-gray-600"></label>
-                <input
-                    type="text"
-                    placeholder="Enter product name"
-                    className="w-full px-1 py-2 text-xs ring ring-gray-400 outline-none rounded-md"
-                />
+                <label className="text-xs text-gray-600">Product Price</label>
+                <input type="text" />
+            </div>
+            <div className="mt-4">
+                <label className="text-xs text-gray-600">Discount Price</label>
+                <input type="text" />
             </div>
         </aside>
     </>;
