85b99c2d7e9e084c3deb065e62d0e8b3b6d93e7e
owais3536 <owais8812@gmail.com>
2026-01-08 05:05:13 +0500

    Add new product fields

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -2,7 +2,35 @@
 const AddProduct = () => {
 
   return (
-    <div>AddProduct</div>
+    <>
+    <div className="fixed top-0 right-0 w-full h-full bg-black/30"></div>
+
+    <aside className="w-[420px] h-full fixed top-0 right-0 bg-white px-4 py-6">
+      <h2 className="text-xl font-semibold">Add Product</h2>
+
+      <div className="mt-6">
+        <label className="text-sm">Product Name:</label>
+        <input 
+          type="text" 
+          className="block mt-1 w-full rounded-sm text-base py-1 px-2 ring ring-gray-400 outline-none"
+        />
+      </div>
+      <div className="mt-4">
+        <label className="text-sm">Original Price:</label>
+        <input 
+          type="number" 
+          className="block mt-1 w-full rounded-sm text-base py-1 px-2 ring ring-gray-400 outline-none"
+        />
+      </div>
+      <div className="mt-4">
+        <label className="text-sm">Discount Price:</label>
+        <input 
+          type="number" 
+          className="block mt-1 w-full rounded-sm text-base py-1 px-2 ring ring-gray-400 outline-none"
+        />
+      </div>
+    </aside>
+    </>
   );
 }
 
